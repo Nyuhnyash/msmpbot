665115535ae22e02db73934287eb5c13192a568e
Nyuhnyash <git@mailing.cf>
2020-07-27 13:23:00 +0400

    Added URL validator and plural endings chooser

--- utils.py
+++ utils.py
@@ -1,10 +1,20 @@
-# -*- coding: utf-8 -*-
-# Guillermo Siesto
-# github.com/GSiesto
-
+from validators import url
 
 def validUrl(url):
-    if int(len(url)) > 35:
+    if url(url) & int(len(url)) > 35:
         return False
     else:
         return True
+
+def plural(n):
+# Source: https://gist.github.com/CubexX/182bd5918d3455d986b354eadaea02ce
+    endings = ['', 'а', 'ов']
+    
+    if n % 10 == 1 and n % 100 != 11:
+        p = 0
+    elif 2 <= n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
+        p = 1
+    else:
+        p = 2
+
+    return str(n) + ' ' + endings[p]
