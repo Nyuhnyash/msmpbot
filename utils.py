2fb4caf46395442d179f99bb49d4febef2a06e3a
Nyuhnyash <git@mailing.cf>
2020-08-02 22:02:28 +0400

    Added update main object specifier

--- utils.py
+++ utils.py
@@ -18,7 +18,18 @@ def ending(n):
 
     return endings[p]
 
-def name_and_id(source):
-    user = source.from_user
-    return "{0} ({1})".format(user.username, user.id)
+def name_and_id(effective_user):
+    return "{0} ({1})".format(effective_user.username, effective_user.id)
+
+update_objects_attrs = { 'inline_query', 'message', 'callback_query'} 
+
+def update_object_type(update):
+    for object_attr in update_objects_attrs:
+        update_object = update.__dict__.get(object_attr, None)
+        if update_object:
+            return update_object
+            
+    raise NotATelegramUpdateInstanseError
+
+class NotATelegramUpdateInstanseError(BaseException): ...
 
