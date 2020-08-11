7f8b3aa8e1d9a7017272168589b43a159a0149e5
Nyuhnyash <git@mailing.cf>
2020-08-11 17:39:32 +0400

    Added enable-query checker using mcsrvstat.us API

--- utils.py
+++ utils.py
@@ -1,12 +1,36 @@
-def validUrl(st):
-    from validators import url
-    if url("http://" +st) and (int(len(st)) < 35):
+from validators import url
+def validUrl(address):
+    if url('http://' + address) and (int(len(address)) < 35):
         return True
     else:
         return False
 
+
+from mcstatus import MinecraftServer as mcS
+from requests import get
+api = 'https://api.mcsrvstat.us/2/'
+
+class MinecraftServer(mcS):
+    def __init__(self, host, port=25565):
+        mcS.__init__(self, host, port)
+        self._enable_query = None
+
+    @staticmethod
+    def lookup(address):
+        server = mcS.lookup(address)
+        return MinecraftServer(server.host, server.port)
+        
+    @property
+    def enable_query(self):
+        if self._enable_query:
+            return self._enable_query
+        else:
+            return get('{}{}:{}'.format(api, self.host, self.port)).json()['debug']['query']
+
+
 def name_and_id(effective_user):
     return "{0} ({1})".format(effective_user.username, effective_user.id)
+
 
 update_objects_attrs = { 'inline_query', 'message', 'callback_query'} 
 
